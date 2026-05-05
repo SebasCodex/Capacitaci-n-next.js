dbd0ea145421a58367f80f68de6cda1a8c55a658
SebasCodex <sebasdefinitivo1@gmail.com>
2026-05-04 23:06:20 -0500

    feat: configurar layout global con Navbar y contenedor principal

--- rv4-next/app/components/Navbar.js
+++ rv4-next/app/components/Navbar.js
@@ -4,10 +4,15 @@ export default function Navbar() {
     return (
         <nav style={{ background: "#222", padding: "10px" }}>
             <ul style={{ display: "flex", gap: "15px", listStyle: "none", margin: 0 }}>
+
                 <li><Link href="/" style={{ color: "#fff" }}>Inicio</Link></li>
+
                 <li><Link href="/dashboard" style={{ color: "#fff" }}>Dashboard</Link></li>
+
                 <li><Link href="/projects" style={{ color: "#fff" }}>Proyectos</Link></li>
+
                 <li><Link href="/students" style={{ color: "#fff" }}>Estudiantes</Link></li>
+
             </ul>
         </nav>
     );
